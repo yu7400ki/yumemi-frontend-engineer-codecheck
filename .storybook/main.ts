4f5dd814830bfdcaaf953e7694112af91d14701a
Yuki Natori <104719412+yu7400ki@users.noreply.github.com>
2025-02-01 22:28:45 +0900

    Chore: Storybookの導入 (#5)
    * storybook
    
    * exclude cloudflare plugin

---
+++ .storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  viteFinal: (config) => {
+    return {
+      ...config,
+      plugins: config.plugins?.filter(
+        // @ts-ignore
+        (plugin) => plugin?.name !== "vite-plugin-cloudflare",
+      ),
+    };
+  },
+};
+
+export default config;
