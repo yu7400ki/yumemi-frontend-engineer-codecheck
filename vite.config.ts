06c9b2b73aff0eeb1aace16c45891c2f637c60a0
Yuki Natori <104719412+yu7400ki@users.noreply.github.com>
2025-02-01 23:06:15 +0900

    Chore: パスエイリアス (#8)
    * add vite-tsconfig-paths
    
    * configure path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react(), cloudflare(), tsconfigPaths()],
 });
