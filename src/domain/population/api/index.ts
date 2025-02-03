e36e3dcaea8e5844afc11824e40e790ae103f2b3
yu7400ki <yu7400ki@gmail.com>
2025-02-04 04:07:42 +0900

    population chart

--- src/domain/population/api/index.ts
+++ src/domain/population/api/index.ts
@@ -1,5 +1,7 @@
 import { HttpError, client } from "@/libs/api";
+import { useQueries } from "@tanstack/react-query";
 import type { GetApiV1PopulationCompositionPerYearParams } from "api/gen/models";
+import { zip } from "es-toolkit";
 
 export async function getPopulation({
   prefCode,
@@ -9,4 +11,33 @@ export async function getPopulation({
     return await resp.json();
   }
   throw new HttpError(resp);
+}
+
+export function usePopulations(prefCodes: number[]) {
+  return useQueries({
+    queries: prefCodes.map((prefCode) => {
+      return {
+        queryKey: ["population", prefCode],
+        queryFn: () => getPopulation({ prefCode: String(prefCode) }),
+      };
+    }),
+    combine: (results) => {
+      return {
+        data: zip(prefCodes, results)
+          .map(([prefCode, result]) =>
+            result.data !== undefined
+              ? {
+                  prefCode,
+                  data: result.data,
+                }
+              : undefined,
+          )
+          .filter((result) => result !== undefined),
+        error: results.map((result) => result.error),
+        isFetching: results.some((result) => result.isFetching),
+        isPending:
+          results.length > 0 && results.every((result) => result.isPending),
+      };
+    },
+  });
 }
