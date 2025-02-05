e1a2ed76ba45913143e2b6ce3ce249178c847acb
yu7400ki <yu7400ki@gmail.com>
2025-02-06 01:51:42 +0900

    loading

--- src/domain/prefecture/components/prefectures-checkbox-group/prefectures-checkbox-group.tsx
+++ src/domain/prefecture/components/prefectures-checkbox-group/prefectures-checkbox-group.tsx
@@ -1,22 +1,11 @@
 import { Checkbox } from "@/components/ui/checkbox";
 import { CheckboxGroup } from "@/components/ui/checkbox-group";
-import { useCallback } from "react";
+import { Suspense, useCallback } from "react";
 import { css } from "styled-system/css";
 import { usePrefectures } from "../../api";
 import { usePrefecturesSelector } from "../../hooks/use-prefectures-selector";
 
 export function PrefecturesCheckboxGroup() {
-  const { data: prefectures } = usePrefectures();
-  const { selectedPrefectureCodes, setSelectedPrefectureCodes } =
-    usePrefecturesSelector();
-
-  const handleChange = useCallback(
-    (prefCode: string[]) => {
-      setSelectedPrefectureCodes(prefCode.map(Number));
-    },
-    [setSelectedPrefectureCodes],
-  );
-
   return (
     <fieldset
       className={css({
@@ -34,18 +23,57 @@ export function PrefecturesCheckboxGroup() {
       >
         都道府県
       </legend>
-      <CheckboxGroup
-        value={selectedPrefectureCodes.map(String)}
-        onChange={handleChange}
-      >
-        {prefectures?.map((prefecture) => (
-          <div key={prefecture.prefCode} className={css({ w: "fit-content" })}>
-            <Checkbox name="prefectures" value={String(prefecture.prefCode)}>
-              {prefecture.prefName}
-            </Checkbox>
-          </div>
-        ))}
-      </CheckboxGroup>
+      <Suspense fallback={<LoadingPrefectures />}>
+        <PrefecturesCheckboxList />
+      </Suspense>
     </fieldset>
+  );
+}
+
+function PrefecturesCheckboxList() {
+  const { data: prefectures } = usePrefectures();
+  const { selectedPrefectureCodes, setSelectedPrefectureCodes } =
+    usePrefecturesSelector();
+
+  const handleChange = useCallback(
+    (prefCode: string[]) => {
+      setSelectedPrefectureCodes(prefCode.map(Number));
+    },
+    [setSelectedPrefectureCodes],
+  );
+
+  return (
+    <CheckboxGroup
+      value={selectedPrefectureCodes.map(String)}
+      onChange={handleChange}
+    >
+      {prefectures?.map((prefecture) => (
+        <div key={prefecture.prefCode} className={css({ w: "fit-content" })}>
+          <Checkbox name="prefectures" value={String(prefecture.prefCode)}>
+            {prefecture.prefName}
+          </Checkbox>
+        </div>
+      ))}
+    </CheckboxGroup>
+  );
+}
+
+function LoadingPrefectures() {
+  return (
+    <>
+      {Array.from({ length: 47 }).map((_, index) => (
+        <div
+          // biome-ignore lint/suspicious/noArrayIndexKey: static array
+          key={index}
+          className={css({
+            w: 20,
+            h: 6,
+            backgroundColor: "gray.200",
+            rounded: "lg",
+            animation: "pulse 2s infinite",
+          })}
+        />
+      ))}
+    </>
   );
 }
