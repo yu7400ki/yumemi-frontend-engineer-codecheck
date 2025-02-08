11ae19e11e71881c4053a96438343e2fea968483
yu7400ki <yu7400ki@gmail.com>
2025-02-08 23:14:43 +0900

    story

--- src/components/pages/index.stories.tsx
+++ src/components/pages/index.stories.tsx
@@ -1,4 +1,7 @@
+import { client } from "@/libs/api";
 import type { Meta, StoryObj } from "@storybook/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { http } from "msw";
 import { Page } from ".";
 
 const meta = {
@@ -14,4 +17,41 @@ export default meta;
 
 type Story = StoryObj<typeof Page>;
 
-export const Default: Story = {};
+function createQueryClient() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+  return queryClient;
+}
+
+function decorators(queryClient: QueryClient): Story["decorators"] {
+  return (Story) => (
+    <QueryClientProvider client={queryClient}>
+      <Story />
+    </QueryClientProvider>
+  );
+}
+
+export const Default: Story = {
+  decorators: decorators(createQueryClient()),
+};
+
+export const OnError: Story = {
+  decorators: decorators(createQueryClient()),
+  parameters: {
+    msw: {
+      handlers: [
+        [
+          http.get(client.prefectures.$url().toString(), () => {
+            return new Response("Internal Server Error", { status: 500 });
+          }),
+        ],
+      ],
+    },
+  },
+};
